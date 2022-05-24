a013e4521aa3adfc7f25e9fd121d4a437c2519a8
Horacio Capdevila <61241842+hor-5@users.noreply.github.com>
2022-05-24 03:34:17 -0300

    'a'

--- src/components/SkeletonCardUser.js
+++ src/components/SkeletonCardUser.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import {Box,SkeletonCircle,SkeletonText} from '@chakra-ui/react'
+import { Box, SkeletonCircle, SkeletonText, Center } from '@chakra-ui/react'
 
 export default function SkeletonCardUser() {
     return (
         <Box padding='6' boxShadow='lg' bg='white'>
-            <SkeletonCircle size='10' />
+            <Center>
+                <SkeletonCircle size='10' />
+            </Center>
             <SkeletonText mt='4' noOfLines={4} spacing='4' />
+
         </Box>
     )
 }
